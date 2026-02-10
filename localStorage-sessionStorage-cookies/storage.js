53a88f04ab097f153de273aeb0cdc1bb242ad3f3
Govind <heyamgovind@gmail.com>
2026-02-11 00:25:49 +0530

    worked on the techleap session's project

---
+++ localStorage-sessionStorage-cookies/storage.js
@@ -0,0 +1,8 @@
+/* store an item = setItem();
+fetch the data = getItem();
+remove the item = removeItem();
+update the item = well, we just use the setItem() method.
+
+*/
+
+let theme = localStorage.getItem('theme');
